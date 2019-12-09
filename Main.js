beae7ea90884178a408cdd95aace7ffbc2048551
maykar <25127328+maykar@users.noreply.github.com>
2019-12-09 23:03:54 +0000

    Deploying to gh-pages from master 4a7279a7fb475cba276106cd2091f0c69628d99f

--- Main.js
+++ Main.js
@@ -4234,13 +4234,13 @@ github.com style (c) Vasily Polovnyov <vast@whiteants.net>
 
   .markdown-body table th {
     font-weight: 600;
+    background: var(--markdown-secondary-background);
   }
 
   .markdown-body table td,
   .markdown-body table th {
     padding: 6px 13px;
     border: 1px solid var(--markdown--divider-color);
-    background: var(--markdown-secondary-background);
   }
 
   .markdown-body table tr {
